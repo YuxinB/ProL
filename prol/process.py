434570fe4e0c1153e10dc9a2b7c89faf7b071a77
Laknath1996 <ashwindesilva@yahoo.com>
2024-05-16 09:26:06 -0400

    Add multi class/task indices

--- prol/process.py
+++ prol/process.py
@@ -1,10 +1,12 @@
 import torch
+import torch.utils
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
 import math
 from copy import deepcopy
 
+# get pattern
 def get_cycle(N: int) -> list:
     """Get the primary cycle
 
@@ -20,6 +22,56 @@ def get_cycle(N: int) -> list:
     """
     return [1] * N + [0] * N
 
+def get_multi_cycle(N, num_tasks):
+    """Get the primary cycle (for multi-task problems)
+
+    Parameters
+    ----------
+    N : int
+        time between two task switches
+    num_tasks : int
+        number of tasks
+
+    Returns
+    -------
+    list
+        primary cycle
+    """
+    flags = np.arange(num_tasks)
+    unit = []
+    for flag in flags:
+        unit += [flag] * N
+    return unit
+
+# synthetic data
+def draw_synthetic_samples(flip):
+    y = np.random.binomial(1, 0.5)
+    l = 1-y if flip else y
+    x = l * np.random.uniform(-2, -1) + (1-l) * np.random.uniform(1, 2)
+    return x, y
+
+def get_synthetic_data(N, total_time_steps, seed=1996):
+    """Get synthetic data sequence drawn from the stochastic process
+
+    Parameters
+    ----------
+    N : time between two task switches
+    total_time_steps : length of the sequence drawn
+    seed : random seed
+
+    Returns
+    -------
+    index sequence
+    """
+    unit = get_cycle(N)
+    pattern = np.array((unit * math.ceil(total_time_steps/(len(unit))))[:total_time_steps]).astype("bool")
+    data = np.zeros((total_time_steps, 2)).astype('float')
+    np.random.seed(seed)
+    data[pattern] = np.array([draw_synthetic_samples(True) for _ in range(sum(pattern))])
+    data[~pattern] = np.array([draw_synthetic_samples(False) for _ in range(sum(~pattern))])
+    return torch.from_numpy(data[:, 0]).float(), torch.from_numpy(data[:, 1]).long()
+
+# vision data
 def get_torch_dataset(root, name='mnist'):
     """Get the original torch datase
 
@@ -56,6 +108,7 @@ def get_torch_dataset(root, name='mnist'):
     assert dataset.data.min() == -1.0
     return dataset
 
+# vision (label swap and covariate shift)
 def get_task_indicies_and_map(tasks: list, y: np.ndarray, type='covariate-shift'):
     """Get the indices for each task + the label mapping
 
@@ -143,34 +196,117 @@ def get_sequence_indices(N, total_time_steps, tasklib, seed=1996, remove_train_s
     else:
         return seqInd
 
-def draw_synthetic_samples(flip):
-    y = np.random.binomial(1, 0.5)
-    l = 1-y if flip else y
-    x = l * np.random.uniform(-2, -1) + (1-l) * np.random.uniform(1, 2)
-    return x, y
+# vision (multi-class and multi-task)
+def flatten(tasks):
+    return [x for task in tasks for x in task]
 
-def get_synthetic_data(N, total_time_steps, seed=1996):
-    """Get synthetic data sequence drawn from the stochastic process
+def get_multi_indices_and_map(tasks, dataset):
+    """Get the indices for each task + the label mapping
+
+    Parameters
+    ----------
+    tasks : list
+        task specification e.g. [[0, 1], [2, 3]]
+    dataset : torch Dataset
+        torch dataset 
+    """
+    y = dataset.targets.numpy()
+
+    # get the number of unique classes in the task spec
+    labels = np.unique(flatten(tasks))
+
+    # get the indices for each unique class in the task spec
+    classlib = {}
+    for lab in labels:
+        classlib[lab] = []
+        classlib[lab].extend(
+            np.where(y == lab)[0].tolist()
+        )
+
+    # get the total number of appearance of each class in the task spec
+    label_usage = []
+    for lab in labels:
+        count = 0
+        for task in tasks:
+            if lab in task:
+                count += 1
+        label_usage.append(count)
+
+    # split the indices of each class according to class usage above
+    for lab, count in zip(labels, label_usage):
+        tmp = np.array(classlib[lab])
+        tmp = np.array_split(tmp, count)
+        classlib[lab] = tmp
+
+    # get the indices corresponding to each task
+    tasklib = {}
+    for i, task in enumerate(tasks):
+        tasklib[i] = []
+        for lab in task:
+            tasklib[i].extend(
+                classlib[lab].pop()
+            )
+
+    # get the label mapper
+    task_labels = [np.arange(len(task)).tolist() for task in tasks]
+    dummy_labels = deepcopy(task_labels)
+    k = 10
+    for i, _ in enumerate(task_labels):
+        for j, _ in enumerate(task_labels[i]):
+            dummy_labels[i][j] = k
+            k += 1
+    mapdict = dict(zip(flatten(dummy_labels), flatten(task_labels)))
+    maplab = lambda lab : mapdict[lab]
+
+    # assign dummy labels to the original labels
+    for i, _ in enumerate(tasks):
+        tmp = y[tasklib[i]]
+        for trulab, dumlab in zip(tasks[i], dummy_labels[i]):
+            tmp[tmp == trulab] = dumlab
+        y[tasklib[i]] = tmp
+
+    dataset.targets = torch.from_numpy(y).long()
+
+    return tasklib, maplab, dataset
+
+def get_multi_sequence_indices(N, total_time_steps, tasklib, seed=1996, remove_train_samples=False):
+    """Get indices for a sequence drawn from the stochastic process
 
     Parameters
     ----------
     N : time between two task switches
     total_time_steps : length of the sequence drawn
+    tasklib : original task indices
     seed : random seed
 
     Returns
     -------
     index sequence
     """
-    unit = get_cycle(N)
-    pattern = np.array((unit * math.ceil(total_time_steps/(len(unit))))[:total_time_steps]).astype("bool")
-    data = np.zeros((total_time_steps, 2)).astype('float')
+    tasklib = deepcopy(tasklib)
+    num_tasks = len(tasklib)
+    unit = get_cycle(N, num_tasks)
+    pattern = np.array((unit * math.ceil(total_time_steps/(len(unit))))[:total_time_steps]).astype("int")
+    seqInd = np.zeros((total_time_steps,)).astype('int')
+
     np.random.seed(seed)
-    data[pattern] = np.array([draw_synthetic_samples(True) for _ in range(sum(pattern))])
-    data[~pattern] = np.array([draw_synthetic_samples(False) for _ in range(sum(~pattern))])
-    return torch.from_numpy(data[:, 0]).float(), torch.from_numpy(data[:, 1]).long()
+    for taskid in range(num_tasks):
+        seqInd[pattern==taskid] = np.random.choice(tasklib[taskid], sum(pattern==taskid), replace=False)
+
+    if remove_train_samples:
+        for taskid in range(num_tasks):
+            tasklib[taskid] = list(
+                set(tasklib[taskid]) - set(tasklib[taskid]).intersection(seqInd)
+            ) 
+        return seqInd, tasklib
+    else:
+        return seqInd
 
 if __name__ == "__main__":
     x, y = get_synthetic_data(20, 100)
     print(x.shape)
+
+
+
+
 
