3a7e4c53f19d66f958c32d7bb1420e8d4d73af00
Rahul Ramesh <rahul13ramesh@gmail.com>
2024-05-18 19:29:43 +0000

    Update

--- prol/models/resnet.py
+++ prol/models/resnet.py
@@ -98,11 +98,19 @@ class Model(nn.Module):
     
 def model_defaults(dataset):
     if dataset == 'cifar-10':
+        # return {
+        #     "depth": 10,
+        #     "base_chans": 4, 
+        #     "input_chans": 3,
+        #     "widen_factor": 2,
+        #     "dropRate": 0
+        # }
+        # new params
         return {
-            "depth": 10,
-            "base_chans": 4, 
+            "depth": 16,
+            "base_chans": 16, 
             "input_chans": 3,
-            "widen_factor": 2,
+            "widen_factor": 4,
             "dropRate": 0
         }
     else:
