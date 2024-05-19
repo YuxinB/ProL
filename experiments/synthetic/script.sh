9e9ca8510638057e6b022478d67a554fbc7adad3
Rahul Ramesh <rahul13ramesh@gmail.com>
2024-05-19 20:25:50 +0000

    Update

--- experiments/synthetic/script.sh
+++ experiments/synthetic/script.sh
@@ -1,10 +1,15 @@
 python run_synthetic.py -m t=100,200,500,700,1000,1200,1500,1700,2000,2500
 # python run.py -m t=500,700,1000,1200,1500,1700,2000,2500
 
+
+# Baseline 1 (Oracle)
+python run_baseline_1.py -m device='cuda:0' method="mlp" epochs=300 batchsize=64 t=0,100,200,500,700,1000,1200,1500,1700,2000,2500 hydra.launcher.n_jobs=8
+
 # MLP
 python run_synthetic.py -m device='cuda:2' method="mlp" epochs=300 batchsize=64 t=0,100,200,500,700,1000,1200,1500,1700,2000,2500 hydra.launcher.n_jobs=8
 
 # time MLP
+python run_synthetic.py -m device='cuda:1' method="timemlp" epochs=500 batchsize=64 t=0,100,200,500,700,1000,1200,1500,1700,2000,2500 hydra.launcher.n_jobs=8
 
 # prosp. transformer
 python run_synthetic.py -m device='cuda:0' method="proformer" multihop=True epochs=300 batchsize=64 t=0,100,200,500,700,1000,1200,1500,1700,2000,2500 hydra.launcher.n_jobs=8
