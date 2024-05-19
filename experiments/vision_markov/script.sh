de6e1f8847e42fd1d0679ccf301671df9a800bcf
Rahul Ramesh <rahul13ramesh@gmail.com>
2024-05-19 13:51:11 +0000

    Update

--- experiments/vision_markov/script.sh
+++ experiments/vision_markov/script.sh
@@ -7,16 +7,16 @@ python run_baseline_1.py -m device='cuda:0' method="cnn" epochs=200 batchsize=64
 python run_baseline_2.py -m device='cuda:1' method="cnn" epochs=100 batchsize=64 hydra.launcher.n_jobs=1
 
 # cnn
-python run_vision_multi.py -m device='cuda:0' method="cnn" epochs=500 batchsize=64 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=8
+python run_vision_markov.py -m device='cuda:0' method="cnn" epochs=500 batchsize=64 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=8
 
 # timecnn
-python run_vision_multi.py -m device='cuda:1' method="timecnn" epochs=500 batchsize=64 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=8
+python run_vision_markov.py -m device='cuda:1' method="timecnn" epochs=500 batchsize=64 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=8
 
 # prosp. transformer
-python run_vision_multi.py -m device='cuda:2' method="proformer" multihop=True epochs=300 batchsize=32 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=6
+python run_vision_markov.py -m device='cuda:2' method="proformer" multihop=True epochs=300 batchsize=32 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=6
 
 # transformer
-python run_vision_multi.py -m device='cuda:3' method="proformer" multihop=False epochs=300 batchsize=32 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=6
+python run_vision_markov.py -m device='cuda:3' method="proformer" multihop=False epochs=300 batchsize=32 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=6
 
 
 # #### CIFAR-10 ####
@@ -28,13 +28,13 @@ python run_baseline_1.py -m device='cuda:0' method="resnet" epochs=1000 batchsiz
 python run_baseline_2.py -m device='cuda:1' method="resnet" epochs=500 batchsize=64 hydra.launcher.n_jobs=1
 
 # resnet
-python run_vision_multi.py -m device='cuda:0' method="resnet" epochs=1000 batchsize=64 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=8
+python run_vision_markov.py -m device='cuda:0' method="resnet" epochs=1000 batchsize=64 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=8
 
 # time-resnet
-python run_vision_multi.py -m device='cuda:1' method="timeresnet" epochs=1000 batchsize=64 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=8
+python run_vision_markov.py -m device='cuda:1' method="timeresnet" epochs=1000 batchsize=64 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=8
 
 # prosp. transformer
-python run_vision_multi.py -m device='cuda:2' method="conv_proformer" multihop=True epochs=300 batchsize=16 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=3
+python run_vision_markov.py -m device='cuda:2' method="conv_proformer" multihop=True epochs=300 batchsize=16 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=3
 
 # transformer
-python run_vision_multi.py -m device='cuda:3' method="conv_proformer" multihop=False epochs=300 batchsize=16 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=3
+python run_vision_markov.py -m device='cuda:3' method="conv_proformer" multihop=False epochs=300 batchsize=16 t=0,200,500,700,1000,1200,1500,1700,2000,2500,3000,4000 hydra.launcher.n_jobs=3
