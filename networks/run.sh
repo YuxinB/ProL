a6038c4601839fccc135e8c786e3db2b4cab0bff
Rahul Ramesh <rahulram@seas.upenn.edu>
2024-07-08 17:02:45 -0400

    fine-tuning code

---
+++ networks/run.sh
@@ -0,0 +1,2 @@
+python3 02_train.py  net.type=mlp name='mlp_ft_p20' tag=scenario2 numseeds=1 tstart=20 tend=2000 tskip=1 train.epochs=20 fine_tune=10 data.bs=8
+# python3 02_train.py  net.type=mlp name='mlp_ft_p20' tag=scenario2 numseeds=1 tstart=20 tend=2000 tskip=1 train.epochs=20 fine_tune=20 data.bs=8 bgd=True
